f839cf28664764a99d60dfb60a1b07bf8ddc6a68
Andres Carrillo <af.carrillo@live.com>
2023-06-26 16:09:04 -0400

    API Map

--- start/routes.ts
+++ start/routes.ts
@@ -22,16 +22,15 @@ import Route from '@ioc:Adonis/Core/Route';
 
 
 Route.group(() => {
-  //Auth
+  //Login
   Route.post('/login', 'UsuariosController.login');
   Route.post('/registrar', 'UsuariosController.registrar');
   Route.get('/logout', 'UsuariosController.logout');
   Route.post('/isAuth', 'UsuariosController.isAuth');
-  //locales
+  //Locales
   Route.post('/locales_activos', 'LocalesController.locales_activos');
   Route.post('/categorias', 'CategoriasLocalesController.categorias');
-  //
-  //sugerir
+  //Auth
   Route.group(() => {
     Route.post('/crear_review', 'ReviewsController.calificar');
     Route.post('/get_url_upload', 'MinioController.getUrlUpload');
@@ -44,5 +43,25 @@ Route.group(() => {
   }).middleware('auth');
 }).prefix('api/v1');
 Route.get('/', async () => {
-  return { hello: 'world' };
+  return `API de pollos App, Prefix: api/v1;
+  //Login
+  Route.post('/login', 'UsuariosController.login');
+  Route.post('/registrar', 'UsuariosController.registrar');
+  Route.get('/logout', 'UsuariosController.logout');
+  Route.post('/isAuth', 'UsuariosController.isAuth');
+  //Locales
+  Route.post('/locales_activos', 'LocalesController.locales_activos');
+  Route.post('/categorias', 'CategoriasLocalesController.categorias');
+  //Auth
+  Route.group(() => {
+    Route.post('/crear_review', 'ReviewsController.calificar');
+    Route.post('/get_url_upload', 'MinioController.getUrlUpload');
+    Route.post('/sugerir', 'LocalesController.sugerir');
+    Route.post('/locales_por_aceptar', 'LocalesController.locales_por_aceptar');
+    Route.post('/aceptar_local', 'LocalesController.aceptar_local');
+    Route.post('/rechazar_local', 'LocalesController.rechazar_local');
+    Route.post('/editar_nombre', 'UsuariosController.editar_nombre');
+    Route.post('/editar_foto_perfil', 'UsuariosController.editar_foto_perfil');
+  }).middleware('auth');
+  `;
 });
